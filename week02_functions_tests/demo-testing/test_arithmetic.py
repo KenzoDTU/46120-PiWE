35b79fd3518bb0196d7fec385b1f7cfc11a44b40
Kenzo Kenzo <161597014+KenzoDTU@users.noreply.github.com>
2026-02-12 11:38:15 +0100

    update

--- week02_functions_tests/demo-testing/test_arithmetic.py
+++ week02_functions_tests/demo-testing/test_arithmetic.py
@@ -1,7 +1,8 @@
 """Check some of the functions in arithmetic.
 """
 from arithmetic import square
-
+import numpy as np
+import pytest
 
 def test_square_integer():
     """Test that the square function returns the correct value for an
@@ -15,7 +16,25 @@ def test_square_integer():
     assert y == y_theo
 
 
+def test_square_float():
+    """Test that the square function returns the correct value for a
+    float input."""
+    # given
+    x = 3.4
+    y_theo = 11.56
+    # when
+    y = square(x)
+    # then
+    #y_theo = pytest.approx(y_theo, abs = 1e-10)
+    #assert y == y_theo
+
+    assert np.isclose(y, y_theo)
+
+
 # code to execute only if Python is executed directly on this module,
 # NOT on import
 if __name__ == '__main__':
     test_square_integer()
+    test_square_float()
+
+
